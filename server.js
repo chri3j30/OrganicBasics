d99bba277731e92f8782fb9542b2238cdda52050
Christine Eenberg <christine@eenberg.com>
2020-03-17 20:36:25 +0100

    languages and repositories mounted

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 var router = express.Router();
 const port = 3000;
 var commits = require('./endpoints/commits.js');
+var languages = require('./endpoints/languages.js');
+var repositories = require('./endpoints/repositories.js')
 //const fetch = require('node-fetch');
 // const apiUrl = 'https://api.github.com/repos/Shopify/shopify_api/commits?since=2017-01-01T19:46:35+0000&until=2017-12-31T19:46:35+0000'
 
@@ -21,3 +23,5 @@ app.listen(port, (err) => {
 });
 
 app.use('/commits', commits);
+app.use('/languages', languages);
+app.use('/repositories', repositories)
