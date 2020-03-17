21c0534410661e8ee5aeaf7277e31e6c9e426f47
Christine Eenberg <christine@eenberg.com>
2020-03-17 15:44:13 +0100

    commits endpoint

---
+++ endpoints/commits.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const fetch = require('node-fetch');
+
+const apiUrl = 'https://api.github.com/repos/Shopify/shopify_api/commits?since=2017-01-01T19:46:35+0000&until=2017-12-31T19:46:35+0000'
+
+router.get('/', (req, res) => {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+
+            var mappedCommits;
+            
+            mappedCommits = data.map(x => {
+                return {
+                    author: x.commit.author.name,
+                    date: x.commit.author.date,
+                    message: x.commit.message
+                }
+            });
+
+            res.send(mappedCommits);
+        })
+        .catch(err => {
+            res.redirect('/error');
+        });
+});
+
+module.exports = router;
+
+
