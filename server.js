ad1f8e566846827ad778580345fc7a475c1f27fd
Christine Eenberg <christine@eenberg.com>
2020-03-17 15:41:15 +0100

    server med commit route

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+
 const app = express();
+var router = express.Router();
 const port = 3000;
+var commits = require('./endpoints/commits.js');
+//const fetch = require('node-fetch');
+// const apiUrl = 'https://api.github.com/repos/Shopify/shopify_api/commits?since=2017-01-01T19:46:35+0000&until=2017-12-31T19:46:35+0000'
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('ok');
 });
 
@@ -13,3 +19,5 @@ app.listen(port, (err) => {
     if(err) { console.log(err); };
     console.log('Listening on port ' + port);
 });
+
+app.use('/commits', commits);
